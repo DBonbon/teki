7ab0cb900f0ef8a06ad032596713fc1a2708d8d0
DBonbon <yrfriedman@gmail.com>
2021-12-22 12:26:54 +0000

    initial comit

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-pz08+%k3!d*0bod(q@3*k@1=#=2bbk+!n(@ph!%9$pes@*p1w1'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
